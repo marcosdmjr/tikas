b1f3d3ebe0980babea195cd5606a0fcc4abcd254
Marcos <marcosdmjr11@gmail.com>
2025-12-05 16:53:07 -0300

    Added upsell.html

--- js/pagamento.js
+++ js/pagamento.js
@@ -230,7 +230,7 @@ function iniciarVerificacaoPagamento(idTransacao) {
 
     if (isPaid) {
       clearInterval(checkPaymentInterval);
-      window.location.href = 'index.html#nine';
+      window.location.href = 'upsell.html';
     }
   }, 5000);
 }
